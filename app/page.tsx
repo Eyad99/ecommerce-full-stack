1e9fb761120134b505be289d4dc91510cf452292
eyad sharaf almasri <iead.sh123@gmail.com>
2025-02-10 18:07:24 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-// import { productApi } from '@/core/services/product';
-// import { Suspense } from 'react';
+import { productApi } from '@/core/services/product';
+import { Suspense } from 'react';
 
 export default async function Home() {
-	// const productsData = await productApi.products();
-	// console.log('products12', productsData);
+	const productsData = await productApi.products();
+	console.log('products12', productsData);
 
 	return (
-		<>test 1</>
-		// <Suspense fallback='loadin'>
-		// 	{productsData?.data?.map((product: any) => (
-		// 		<h6 key={product.id}>{product.name}</h6>
-		// 	))}
-		// </Suspense>
+		<Suspense fallback='loadin'>
+			{productsData?.data?.map((product: any) => (
+				<h6 key={product.id}>{product.name}</h6>
+			))}
+		</Suspense>
 	);
 }
