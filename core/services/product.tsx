0d09028867a6e51882cadd3943fda2f1f50ac43a
eyad sharaf almasri <iead.sh123@gmail.com>
2025-02-11 17:02:49 +0300

    force-dynamic

--- core/services/product.tsx
+++ core/services/product.tsx
@@ -1,6 +1,9 @@
 import { get } from '@/utils/api';
 
+export const dynamic = 'force-dynamic'; // Force dynamic rendering
+
 export const productApi = {
-	// products: async () => get(`products`, { cache: 'no-store' }),
-	products: async () => get(`products`, { next: { revalidate: 10 } }),
+	// products: async () => get(`products`, { cache: 'reload'}),
+	// products: async () => get(`products`, { next: { revalidate: 10 } }),
+	products: async () => get(`products`),
 };
